3a79136b10f314d2f54cba2c100b128dd52585be
JihoonSon94 <jihoon1708@gmail.com>
2024-10-11 10:40:47 +0900

    24.10.11

--- VIG 2.1/VIG.2.1.py
+++ VIG 2.1/VIG.2.1.py
@@ -19,176 +19,41 @@ class IncarGeneratorApp(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
-    def initUI(self):
-        self.setWindowTitle("INCAR Generator")
-        self.setGeometry(500, 100, 800, 800)
-        self.setFixedSize(800, 800)
-
-        self.tabs = QTabWidget(self)
-        self.tabs.setGeometry(10, 10, 780, 780)
-
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        self.tab3 = QWidget()
-        self.tab4 = QWidget()
-        self.tab5 = QWidget()
-        self.tab6 = QWidget()
-
-        self.tabs.addTab(self.tab1, "INCAR")
-        self.tabs.addTab(self.tab2, "POSCAR")
-        self.tabs.addTab(self.tab3, "KPOINTS")
-        self.tabs.addTab(self.tab4, "Shell")
-        self.tabs.addTab(self.tab5, "File Convertor")
-        self.tabs.addTab(self.tab6, "Visualization")
-
-        self.initTab1()
-        self.initTab2()
-        self.initTab3()
-        self.initTab4()
-        self.initTab5()
-        self.initTab6()
-
-    def initTab1(self):
-        layout = QVBoxLayout()
-
-        self.text1 = QTextEdit(self.tab1)
-        layout.addWidget(self.text1)
-
-        model_label = QLabel("Model Type:", self.tab1)
-        layout.addWidget(model_label)
-        self.model_combobox = QComboBox(self.tab1)
-        self.model_combobox.addItems(["Bulk", "Surface", "Molecule"])
-        layout.addWidget(self.model_combobox)
-
-        calculation_label = QLabel("Calculation Type:", self.tab1)
-        layout.addWidget(calculation_label)
-        self.calculation_combobox = QComboBox(self.tab1)
-        self.calculation_combobox.addItems(["Relaxation", "Single Point", "Charge", "Vibration", "DOS", "HSE", "Solvation"])
-        layout.addWidget(self.calculation_combobox)
-
-        functional_label = QLabel("Functional:", self.tab1)
-        layout.addWidget(functional_label)
-        self.functional_combobox = QComboBox(self.tab1)
-        self.functional_combobox.addItems(["PBE", "RPBE", "PW91"])
-        layout.addWidget(self.functional_combobox)
-
-        cutoff_label = QLabel("Cut-off Energy:", self.tab1)
-        layout.addWidget(cutoff_label)
-        self.cutoff_combobox = QComboBox(self.tab1)
-        self.cutoff_combobox.addItems(["400", "500", "600", "700"])
-        layout.addWidget(self.cutoff_combobox)
-
-        vdw_label = QLabel("VDW Correction:", self.tab1)
-        layout.addWidget(vdw_label)
-        self.vdw_combobox = QComboBox(self.tab1)
-        self.vdw_combobox.addItems(["None", "D2", "D3"])
-        layout.addWidget(self.vdw_combobox)
-
-        self.apply_button = QPushButton("Apply", self.tab1)
-        self.apply_button.clicked.connect(self.apply_incar_value)
-        layout.addWidget(self.apply_button)
-
-        self.tab1.setLayout(layout)
-
-    def initTab2(self):
-        layout = QVBoxLayout()
-
-        self.text2 = QTextEdit(self.tab2)
-        layout.addWidget(self.text2)
-
-        self.import_button = QPushButton("Import", self.tab2)
-        self.import_button.clicked.connect(self.read_poscar)
-        layout.addWidget(self.import_button)
-
-        z_label = QLabel("Z value:", self.tab2)
-        layout.addWidget(z_label)
-        self.z_value_entry = QLineEdit(self.tab2)
-        layout.addWidget(self.z_value_entry)
-
-        self.z_value_apply_btn = QPushButton("Apply value", self.tab2)
-        self.z_value_apply_btn.clicked.connect(self.fix_atoms)
-        layout.addWidget(self.z_value_apply_btn)
-
-        self.save_poscar_btn = QPushButton("Save as File", self.tab2)
-        self.save_poscar_btn.clicked.connect(self.write_poscar)
-        layout.addWidget(self.save_poscar_btn)
-
-        self.view_structure_btn = QPushButton("View", self.tab2)
-        self.view_structure_btn.clicked.connect(self.structure_view)
-        layout.addWidget(self.view_structure_btn)
-
-        self.tab2.setLayout(layout)
-
-    def initTab3(self):
-        layout = QVBoxLayout()
-
-        self.text3 = QTextEdit(self.tab3)
-        layout.addWidget(self.text3)
-
-        model_label = QLabel("Model Type:", self.tab3)
-        layout.addWidget(model_label)
-        self.kpoints_combobox = QComboBox(self.tab3)
-        self.kpoints_combobox.addItems(["Bulk", "Surface", "Molecule"])
-        layout.addWidget(self.kpoints_combobox)
-
-        self.kpoints_apply_btn = QPushButton("Apply", self.tab3)
-        self.kpoints_apply_btn.clicked.connect(self.write_kpoints)
-        layout.addWidget(self.kpoints_apply_btn)
-
-        self.tab3.setLayout(layout)
-
-    def initTab4(self):
-        layout = QVBoxLayout()
-
-        self.shell_text = QTextEdit(self.tab4)
-        layout.addWidget(self.shell_text)
-
-        server_label = QLabel("Select Server:", self.tab4)
-        layout.addWidget(server_label)
-        self.shell_combobox = QComboBox(self.tab4)
-        self.shell_combobox.addItems(["CEMD"])
-        layout.addWidget(self.shell_combobox)
-
-        self.shell_apply_btn = QPushButton("Apply", self.tab4)
-        self.shell_apply_btn.clicked.connect(self.make_shell)
-        layout.addWidget(self.shell_apply_btn)
-
-        self.tab4.setLayout(layout)
-
-    def initTab5(self):
-        layout = QVBoxLayout()
-
-        self.drop_label = QLabel("Drag and drop a .cif or .vasp file here", self.tab5)
-        layout.addWidget(self.drop_label)
-
-        self.tab5.setAcceptDrops(True)
-        self.tab5.dragEnterEvent = self.dragEnterEvent
-        self.tab5.dropEvent = self.drop_for_file_convert
-
-        self.tab5.setLayout(layout)
-
-    def initTab6(self):
-        layout = QVBoxLayout()
-
-        self.visualization_import_btn = QPushButton("Import", self.tab6)
-        self.visualization_import_btn.clicked.connect(self.visualization)
-        layout.addWidget(self.visualization_import_btn)
-
-        self.drop_label_visualization = QLabel("Drag and drop a file here", self.tab6)
-        layout.addWidget(self.drop_label_visualization)
-
-        self.tab6.setAcceptDrops(True)
-        self.tab6.dragEnterEvent = self.dragEnterEvent
-        self.tab6.dropEvent = self.visualization_drop
-
-        self.tab6.setLayout(layout)
+        self.setAcceptDrops(True)
+        self.view_button.clicked.connect(self.structure_view)
+        self.z_value_apply.clicked.connect(self.fix_atoms)
+        self.textbox_poscar.dragEnterEvent = self.poscar_drag_enter_event
+        self.textbox_poscar.dropEvent = self.poscar_drop_event
+        self.z_value.returnPressed.connect(self.fix_atoms)
 
     def apply_incar_value(self):
         self.text1.clear()
         # General
-        self.text1.
-        # Implementation for applying INCAR values goes here
+        general = ["#General","PREC    = Accurate","SYMPREC = 1E-8","ISTART  = 0","ICHARG  = 2","ISPIN   = 2","\n"]
+        if combobox_model_type == 
+
+    def poscar_drag_enter_event(self, event):
+        if event.mimeData().hasUrls() :
+            event.accept()
+        else:
+            event.ignore()
+
+    def poscar_drop_event(self, event):
+        if event.mimeData().hasUrls() :
+            self.drop_for_tab2(event)
+        else:
+            event.ignore()
+
+    def drop_for_tab2(self, event):
+        for url in event.mimeData().urls():
+            file_path = url.toLocalFile()
+            clean_path = file_path.strip('{}')
+            atoms = read(clean_path)
+            output = StringIO()
+            write(output, atoms, format='vasp', sort=True)
+            vasp_content = output.getvalue()
+            self.textbox_poscar.clear()
+            self.textbox_poscar.insertPlainText(vasp_content)
 
     def read_poscar(self):
         global Recent_used_directory
@@ -196,27 +61,66 @@ class IncarGeneratorApp(QMainWindow, form_class):
         if filename:
             with open(filename, 'r') as file:
                 file_content = file.read()
-            self.text2.setText(file_content)
+            self.textbox_poscar.setText(file_content)
             Recent_used_directory = os.path.dirname(filename)
 
     def fix_atoms(self):
-        # Implementation for fixing atoms based on Z value
-        pass
+        lines = self.textbox_poscar.toPlainText().splitlines()
+        print(lines)
+        number_of_each_atoms = lines[6].split()
+        total_num_of_atom = sum([int(num) for num in number_of_each_atoms])
+
+        if len(lines) > 7 and lines[7].lower().startswith("s" or "S"):
+            # Replace line 8 with "Selective dynamics"
+            lines[7] = "Selective dynamics"
+        else:
+            # Insert "Selective dynamics" at line 8
+            lines.insert(7, "Selective dynamics")
+
+        input_value = float(self.z_value.text())
+        position_range = total_num_of_atom + 9
+
+        # Modify atom positions based on z_value
+        for i in range(9, position_range):
+            atom_position_list = lines[i].split()
+            atom_position_string = ""
+
+            for j in range(3):
+                if float(atom_position_list[j]) < 0 or float(atom_position_list[j]) >= 10:
+                    atom_position_string += " " + "{:.16f}".format(float(atom_position_list[j]))
+                else:
+                    atom_position_string += "  " + "{:.16f}".format(float(atom_position_list[j]))
+
+            if float(atom_position_list[2]) >= input_value:
+                new_line = atom_position_string + "   T   T   T"
+            else:
+                new_line = atom_position_string + "   F   F   F"
+
+            lines[i] = new_line
+
+        updated_text = "\n".join(lines)
+        self.textbox_poscar.clear()
+        self.textbox_poscar.setPlainText(updated_text)
 
     def write_poscar(self):
         global Recent_used_directory
         file_path, _ = QFileDialog.getSaveFileName(self, "Save POSCAR File", Recent_used_directory, "VASP files (*.vasp);;All files (*.*)")
         if file_path:
-            content = self.text2.toPlainText()
+            content = self.textbox_poscar.toPlainText()
             string_io = StringIO(content)
             atoms = read(string_io, format='vasp')
             write(file_path, atoms)
 
     def structure_view(self):
-        structure_content = self.text2.toPlainText()
+        structure_content = self.textbox_poscar.toPlainText()
         structure_io = StringIO(structure_content)
         visualize = read(structure_io, format='vasp')
-        view(visualize)
+        visualize.edit()
+        output = StringIO()
+        write(output, visualize, format='vasp', sort=True)
+        vasp_content = output.getvalue()
+        self.textbox_poscar.clear()
+        self.textbox_poscar.insertPlainText(vasp_content)
 
     def write_kpoints(self):
         # Implementation for writing k-points
@@ -225,12 +129,6 @@ class IncarGeneratorApp(QMainWindow, form_class):
     def make_shell(self):
         # Implementation for generating shell script
         pass
-
-    def visualization(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open File for Visualization", Recent_used_directory, "VASP files (*.vasp);;All files (*.*)")
-        if filename:
-            visualize = read(filename)
-            view(visualize)
 
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
@@ -243,12 +141,6 @@ class IncarGeneratorApp(QMainWindow, form_class):
                 self.convert_cif2vasp(file_path)
             elif file_path.lower().endswith('.vasp'):
                 self.convert_vasp2cif(file_path)
-
-    def visualization_drop(self, event: QDropEvent):
-        for url in event.mimeData().urls():
-            file_path = url.toLocalFile()
-            visualize = read(file_path)
-            view(visualize)
 
     def convert_cif2vasp(self, file_path):
         if file_path:
